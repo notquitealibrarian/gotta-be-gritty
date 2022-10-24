04885f5b50ce4c39bd357905479f3284ee4950b5
Brett Edmonds <notquitealibrarian@gmail.com>
2022-10-24 14:07:11 -0500

    Updated views and urls to generic list and detail views for blog app.

--- gottabegritty/blog/urls.py
+++ gottabegritty/blog/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.PostList.as_view(), name='index'),
+    path('<slug:slug>/', views.PostDetail.as_view(), name='post')
 ]
--- gottabegritty/blog/views.py
+++ gottabegritty/blog/views.py
@@ -1,5 +1,10 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.views import generic
+from .models import Post
 
-def index(request):
-    return HttpResponse("The blog is up and running!  So much to do...")
+class PostList(generic.ListView):
+    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    template_name = 'index.html'
+
+class PostDetail(generic.DetailView):
+    model = Post
+    template_name = 'post_detail.html'
